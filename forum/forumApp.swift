8d2956e0eb1ba603bc6410e1110f5973e303ae15
Atsushi <atsushi@moongift.jp>
2021-08-02 08:54:06 +0900

    Initial Commit

---
+++ forum/forumApp.swift
@@ -0,0 +1,17 @@
+//
+//  forumApp.swift
+//  forum
+//
+//  Created by Atsushi on 2021/08/02.
+//
+
+import SwiftUI
+
+@main
+struct forumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
